52af25b12f0ce834c5db8e35eafe1f36e7ec6a6e
E. Fine <immortalisEL@gmail.com>
2022-08-08 13:07:48 +0300

    Dockerized Backend (app + db) (removed old structure)

--- src/util/util.module.ts
+++
@@ -1,8 +0,0 @@
-import { UtilService } from './util.service';
-import { Module } from '@nestjs/common';
-
-@Module({
-    providers: [UtilService],
-    exports: [UtilService],
-})
-export class UtilModule {}
--- src/util/util.service.ts
+++
@@ -1,5 +0,0 @@
-export class UtilService {
-    public getTimeElapsedInSeconds(startTime: Date, endTime: Date): number {
-        return Math.round((endTime.getTime() - startTime.getTime()) / 1000);
-    }
-}
